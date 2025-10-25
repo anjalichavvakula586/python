351d5870426b1bc912eb078f17c89c6251b439ed
anjalichavvakula586 <anjalichavvakula5@gmail.com>
2025-10-25 06:19:02 +0530

    first commit

---
+++ variable.py
@@ -0,0 +1,23 @@
+#1.
+num1,num2=100,200
+print(num1+num2,num1-num2,num1*num2,num1/num2,num1//num2,num1%num2,num1**num2)
+
+
+#2.
+
+
+num1,num2=100,200
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1//num2)
+print(num1%num2)
+
+
+
+#3.
+num1,num2=100,200
+
+print("addition ="+str(num1+num2) , "subtraction =" +str(num1-num2) , "multiplication = " +str(num1*num2), "division =" +str(num1/num2))
+
